0de5e57011fc4142ae989ff6b0ff0978ac0b9c37
Donenfeld, Yaniv <donenfel@amazon.com>
2016-06-16 14:05:43 +0300

    build trigger

--- application.py
+++ application.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# changing this comment for demo commi againt
+# changing this comment for demo commi againt again
 
 import os
 import sys
